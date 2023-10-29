fbd66176c8feba6f3709d9773c605546e78b05be
Cat <denperidge@gmail.com>
2023-10-29 10:03:49 +0100

    Run.sh now checks out old version of divio-docs-gen

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/Denperidge-Redpencil/divio-docs-gen.git
 cp .env divio-docs-gen/
 cd divio-docs-gen
+git checkout 43a56393f8aa872abd705aab26ad286c9646b4cb
 python3 -m pip install -r requirements.txt
 python3 app/index.py
